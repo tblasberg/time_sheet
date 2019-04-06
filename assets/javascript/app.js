17a6705a4b58756a3d825e8f6c6c0d6618251157
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-04-06 10:18:51 -0400

    javascript changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,27 @@
 $(document).ready();
 var config = {
-    apiKey: "AIzaSyBob15vJF-aHVOkIJ2eGf_E7kW7pe4mBCo",
-    authDomain: "time-sheet-baa68.firebaseapp.com",
-    databaseURL: "https://time-sheet-baa68.firebaseio.com",
-    projectId: "time-sheet-baa68",
-    storageBucket: "time-sheet-baa68.appspot.com",
-    messagingSenderId: "969988699336"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyBob15vJF-aHVOkIJ2eGf_E7kW7pe4mBCo",
+  authDomain: "time-sheet-baa68.firebaseapp.com",
+  databaseURL: "https://time-sheet-baa68.firebaseio.com",
+  projectId: "time-sheet-baa68",
+  storageBucket: "time-sheet-baa68.appspot.com",
+  messagingSenderId: "969988699336"
+};
+firebase.initializeApp(config);
 var database = firebase.database();
 var newHtmlRow = [];
 
-$(document).on("click", "button", function(){
-
+$(document).on("click", "button", function() {
+  $("#employeeName")
+    .val()
+    .trim();
+  $("#inputRole")
+    .val()
+    .trim();
+  $("#inputStartDate")
+    .val()
+    .trim();
+  $("inputMonthlyRate")
+    .val()
+    .trim();
 });
